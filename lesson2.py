c55af8051a686bbd5ffb589cd22cb6a8e5896488
arkar <arkarkocka@gmail.com>
2020-11-23 22:15:54 +0300

    home work

---
+++ lesson2.py
@@ -0,0 +1,24 @@
+#10
+# str='12345'
+# a = int(str)      #целое число
+# b = float(str)    #вещественное число
+# print(str, a, b)
+
+
+#11
+# str='The real war does not resemble the legendary war in its process or its conclusion.'
+# a = int(len(str))
+# b = int(a/2)
+# print(a, b)
+# part1 = str[0:41]
+# part2 = str[42:83]
+# print(part2,part1)
+
+#12
+#посчитайте количество слов в тексте (вспомните, каким методом можно превратить строку в список);
+# str='The real war does not resemble the legendary war in its process or its conclusion. If it had inspired or directed the development of the legend, then certainly the Ring would have been seized and used against Sauron; he would not have been annihilated but enslaved, and Barad-dûr would not have been destroyed but occupied. Saruman, failing to get possession of the Ring, would in the confusion and treacheries of the time have found in Mordor the missing links in his own researches into Ring-lore, and before long he would have made a Great Ring of his own with which to challenge the self-styled Ruler of Middle-earth. In that conflict both sides would have held hobbits in hatred and contempt: they would not long have survived even as slaves.'
+# a = str.count('a')   #count letter a
+# print(a)
+# str2 = str.replace('a', '@')
+# print(str2)          #replace a to @
+
